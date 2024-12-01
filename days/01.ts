a9772ebfd7d550d466a042766d936e14209b9b9a
Alejandro Baez <develop@a.baez.link>
2024-12-01 10:52:38 -0600

    feat: add boilerplate and test suite for day 1

--- days/01.ts
+++ days/01.ts
@@ -1,5 +1,48 @@
 import { Answer } from "../src/types.ts";
 
+export interface Part1Data {
+  first: Array<number>;
+  second: Array<number>;
+}
+
+interface QuestionOptions {
+  part1Data: Part1Data;
+}
+
+/** Store question part1 */
+export class Question {
+  /** the initial data for part 1 */
+  readonly part1Data: Part1Data;
+
+  /**
+   * @param file the file to read
+   * @param options data to use or other options
+   */
+  constructor(file: string, options?: QuestionOptions) {
+    if (options !== undefined) {
+      this.part1Data = options.part1Data;
+    } else {
+      this.part1Data = this.readPart1(file);
+    }
+  }
+
+  /**
+   * Reads a file provided
+   * @param file the file to read for data
+   */
+  private readPart1(file: string): Part1Data {
+    return {
+      first: [],
+      second: [],
+    };
+  }
+
+  /** get distance sum for the array */
+  distanceSum(): number {
+    return 0;
+  }
+}
+
 /** Provides result output of day done */
 export function result(): Answer {
   return {};
--- days/01_test.ts
+++ days/01_test.ts
@@ -1,6 +1,36 @@
 import { assertEquals } from "jsr:@std/assert";
-import { result } from "./01.ts";
+import { Part1Data, Question, result } from "./01.ts";
+
+const SampleOne: Part1Data = {
+  first: [3, 4, 2, 1, 3, 3],
+  second: [4, 3, 5, 3, 9, 3],
+};
 
 Deno.test("can get result", () => {
   assertEquals(result(), {});
+});
+
+Deno.test("can get distance", () => {
+  const expect = 11;
+
+  const question = new Question("", {
+    part1Data: SampleOne,
+  });
+
+  const actual = question.distanceSum();
+
+  assertEquals(expect, actual);
+});
+
+Deno.test("can read data", () => {
+  const expect: Part1Data = {
+    first: [],
+    second: [],
+  };
+
+  const question = new Question("", {
+    part1Data: SampleOne,
+  });
+
+  assertEquals(expect, question.part1Data);
 });
