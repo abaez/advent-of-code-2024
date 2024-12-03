52f76f37387737e6fd77a1e88d6a5c324a87b9b0
Alejandro Baez <develop@a.baez.link>
2024-12-02 21:16:34 -0600

    feat: add initial load file day2

--- days/02/02.ts
+++ days/02/02.ts
@@ -1,4 +1,90 @@
-import { Answer } from "../../src/types.ts";
+import { Answer, open } from "../../src/types.ts";
+
+/** Direction of safety */
+enum Direction {
+  Inc,
+  Dec,
+}
+
+/** Identifies what each row should contain */
+interface RowType {
+  raw: Array<number>;
+  /** what direction to identify safety */
+  direction?: Direction;
+  /** whether a row is safe or not */
+  safe: boolean;
+}
+
+/** Implemtns row type */
+class Row implements RowType {
+  raw: Array<number>;
+  safe = true;
+  direction?: Direction;
+
+  /**
+   * @param line the raw string to produce a Row from
+   */
+  constructor(line: string) {
+    line.split(" ").map((value, idx, arr) => {
+      const first = parseInt(value);
+      const second = parseInt(arr[idx + 1]);
+
+      if (!isNaN(second)) {
+        const direction = this.directionType(first, second);
+
+        if (this.direction == undefined) {
+          this.direction = direction;
+        } else if (direction != this.direction) {
+          this.safe = false;
+        }
+      }
+
+      // always make sure to write as long as a number
+      if (!isNaN(first)) this.raw[idx] = first;
+    });
+  }
+
+  /**
+   * Find direction and set value
+   * @param first the first number to check
+   * @param second the second number to check
+   */
+  private directionType(first: number, second: number): Direction {
+    return first > second ? Direction.Dec : Direction.Inc;
+  }
+}
+
+export interface DataPart1 {
+  data: Array<Row>;
+}
+
+/** Question logic */
+export class Question {
+  /** @field part1 data from part 1 */
+  readonly part1: DataPart1;
+
+  /**
+   * @param file the file to read for data
+   */
+  constructor(file: string) {
+    this.part1 = this.readPart1(file);
+  }
+
+  /**
+   * Reads the part1 file provided
+   * @param file  the file to read for data
+   */
+  private readPart1(file: string): DataPart1 {
+    const output: DataPart1 = {
+      data: [],
+    };
+
+    for (const line of open(file)) {
+      output.data.push(new Row(line));
+    }
+    return output;
+  }
+}
 
 /** Provides result output of day done */
 export function result(): Answer {
--- days/02/02_test.ts
+++ days/02/02_test.ts
@@ -1,6 +1,18 @@
 import { assertEquals } from "jsr:@std/assert";
-import { result } from "./02.ts";
+import { Question, result } from "./02.ts";
+
+const sample = "days/02/02_sample.txt";
 
 Deno.test("can get result", () => {
   assertEquals(result(), {});
+});
+
+Deno.test("can read data", () => {
+  const expect = [7, 6, 4, 2, 1];
+
+  const question = new Question(sample);
+
+  const actual = question.part1.data[0].raw;
+
+  assertEquals(actual, expect);
 });
