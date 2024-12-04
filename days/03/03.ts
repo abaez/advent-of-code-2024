1da6a487e9cff6ecf03b80aa286417d52485e41d
Alejandro Baez <develop@a.baez.link>
2024-12-03 22:58:08 -0600

    feat: initial add of section construct

--- days/03/03.ts
+++ days/03/03.ts
@@ -4,3 +4,33 @@ import { Answer } from "../../src/types.ts";
 export function result(): Answer {
   return {};
 }
+
+/**
+ * a section of corrupted data
+ */
+export class Section {
+  private row: Array<number>;
+
+  constructor(line: string) {
+    const mulAll = new RegExp(/mul\(\d+,\d+\)/, "g");
+    const mul = new RegExp(/(\d+,\d+)/);
+
+    line.match(mulAll)?.map((match) => {
+      console.log(match);
+      // get numbers if match
+      const numbers = line
+        .match(mul)?.[0]
+        .split(",")
+        .map((rawNumber) => parseInt(rawNumber));
+
+      if (numbers != undefined) {
+        this.row.push(numbers[0] * numbers[1]);
+      }
+    });
+  }
+
+  /** retrieve sum of all values */
+  sum(): number {
+    return 0;
+  }
+}
