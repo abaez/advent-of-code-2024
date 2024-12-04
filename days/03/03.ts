eabf9cb07a3ec0d6b00c1ba7049dc364ced3024e
Alejandro Baez <develop@a.baez.link>
2024-12-03 23:09:51 -0600

    fix: set match based on regex

--- days/03/03.ts
+++ days/03/03.ts
@@ -9,16 +9,20 @@ export function result(): Answer {
  * a section of corrupted data
  */
 export class Section {
-  private row: Array<number>;
+  /** internal representation for a row */
+  private row: Array<number> = [];
 
+  /**
+   * @param line the section line to parse
+   */
   constructor(line: string) {
+    // g used for grouping all
     const mulAll = new RegExp(/mul\(\d+,\d+\)/, "g");
     const mul = new RegExp(/(\d+,\d+)/);
 
     line.match(mulAll)?.map((match) => {
-      console.log(match);
-      // get numbers if match
-      const numbers = line
+      // get numbers if match by splitting and parsing the numbers
+      const numbers = match
         .match(mul)?.[0]
         .split(",")
         .map((rawNumber) => parseInt(rawNumber));
@@ -31,6 +35,8 @@ export class Section {
 
   /** retrieve sum of all values */
   sum(): number {
-    return 0;
+    return this.row.length == 0
+      ? 0
+      : this.row.reduce((sum, value) => sum + value);
   }
 }
