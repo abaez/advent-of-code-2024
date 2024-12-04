f9dcec1bf8b8668dbbdb36cc39ddf14d9082b943
Alejandro Baez <develop@a.baez.link>
2024-12-03 22:57:46 -0600

    test: add tests for mul

--- days/03/03_test.ts
+++ days/03/03_test.ts
@@ -1,6 +1,41 @@
 import { assertEquals } from "jsr:@std/assert";
-import { result } from "./03.ts";
+import { result, Section } from "./03.ts";
+
+const SampleData =
+  "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+const NegativeData = "mul(4*, mul(6,9!, ?(12,34), or mul ( 2 , 4 )";
+
+const MulCheck = "mul(44,46)";
 
 Deno.test("can get result", () => {
   assertEquals(result(), {});
+});
+
+Deno.test("can get multiple", () => {
+  const expect = 161;
+
+  const row = new Section(SampleData);
+
+  const actual = row.sum();
+
+  assertEquals(actual, expect);
+});
+
+Deno.test("can process mul", () => {
+  const expect = 2024;
+
+  const row = new Section(MulCheck);
+
+  const actual = row.sum();
+  assertEquals(actual, expect);
+});
+
+Deno.test("should not multiply", () => {
+  const expect = 0;
+
+  const row = new Section(NegativeData);
+  const actual = row.sum();
+
+  assertEquals(actual, expect);
 });
