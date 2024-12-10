efa97376dc6acbd44aa3f83fd044764a85c5b5b9
Alejandro Baez <develop@a.baez.link>
2024-12-09 21:20:54 -0600

    feat: prepare data structure

--- days/05/05.ts
+++ days/05/05.ts
@@ -1,19 +1,87 @@
 import { Answer, open } from "../../src/types.ts";
 
+/** set of number to match */
+type PageSet = [x: number, y: number];
+
+/** assign page ordering */
+type PageSets = Array<PageSet>;
+
+/** Order of updates */
+type PageUpdates = Array<Array<number>>;
+
 /** Provides result output of day done */
 export function result(): Answer {
   return {};
 }
 
+/** Question abstract for answers */
 export class Question {
+  /** the structure for pages lookup */
+  pages: Pages;
+
   /**
    * @param file the file to read
    */
   constructor(file: string) {
-    open(file);
+    const raw = open(file);
+    this.pages = new Pages(raw);
   }
 
   sum(): number {
     return 0;
+  }
+}
+
+/**
+ * Pages to be checked
+ */
+class Pages {
+  /** the page sets used for figuring order */
+  sets: PageSets = [];
+  /** what are the updates of pages */
+  updates: PageUpdates = [];
+
+  /**
+   * @param rawData  data ingested and formatted accordingly
+   */
+  constructor(rawData: string[]) {
+    for (const line of rawData) {
+      const maybeSet = this.getSet(line);
+      if (maybeSet !== null) {
+        this.sets.push(maybeSet);
+        continue;
+      }
+
+      const maybeUpdate = this.getUpdate(line);
+      if (maybeUpdate !== null) {
+        this.updates.push(maybeUpdate);
+      }
+    }
+  }
+
+  /**
+   * Gets a pageset if exists
+   * @param line the string be validated
+   */
+  getSet(line: string): PageSet | null {
+    const result = line
+      .match(/(\d+)\|(\d+)/g)
+      ?.map((num) => parseInt(num));
+
+    return (result == undefined) ? null : [result[0], result[1]];
+  }
+
+  /**
+   * Gets an update line if exists
+   * @param line the string line to be validated
+   */
+  getUpdate(line: string): Array<number> | null {
+    if (line.match(/,/)?.[0] != undefined) {
+      return line
+        .split(",")
+        .map((num) => parseInt(num));
+    }
+
+    return null;
   }
 }
--- days/05/05_test.ts
+++ days/05/05_test.ts
@@ -1,6 +1,8 @@
 import { assertEquals } from "jsr:@std/assert";
 import { Question, result } from "./05.ts";
 
+const sample = "./samples/05.txt";
+
 Deno.test("can get result", () => {
   assertEquals(result(), {});
 });
@@ -8,9 +10,20 @@ Deno.test("can get result", () => {
 Deno.test("get proper page", () => {
   const expect = 143;
 
-  const question = new Question("./samples/05.txt");
+  const question = new Question(sample);
 
   const actual = question.sum();
 
   assertEquals(actual, expect);
+});
+
+Deno.test("can get input properly", () => {
+  const expect = [21, 6];
+
+  const question = new Question(sample).pages;
+
+  const actual = [question.sets.length, question.updates.length];
+
+  assertEquals(actual[0], expect[0], "sets of x and y");
+  assertEquals(actual[1], expect[1], "updates to run");
 });
