81f5426a671e9f4f7483acb2474412c89c1ca23a
Alejandro Baez <develop@a.baez.link>
2024-12-09 20:25:55 -0600

    feat: introduce day 5 part 1 template

--- days/05/05.ts
+++ days/05/05.ts
@@ -1,6 +1,19 @@
-import { Answer } from "../../src/types.ts";
+import { Answer, open } from "../../src/types.ts";
 
 /** Provides result output of day done */
 export function result(): Answer {
   return {};
+}
+
+export class Question {
+  /**
+   * @param file the file to read
+   */
+  constructor(file: string) {
+    open(file);
+  }
+
+  sum(): number {
+    return 0;
+  }
 }
--- days/05/05_test.ts
+++ days/05/05_test.ts
@@ -1,6 +1,16 @@
 import { assertEquals } from "jsr:@std/assert";
-import { result } from "./05.ts";
+import { Question, result } from "./05.ts";
 
 Deno.test("can get result", () => {
   assertEquals(result(), {});
+});
+
+Deno.test("get proper page", () => {
+  const expect = 143;
+
+  const question = new Question("./samples/05.txt");
+
+  const actual = question.sum();
+
+  assertEquals(actual, expect);
 });
