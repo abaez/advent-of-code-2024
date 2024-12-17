972bcf3b06ef38dac32f72554a89280ed74d4752
Alejandro Baez <develop@a.baez.link>
2024-12-17 00:02:57 -0600

    feat: initial start of day6

--- days/06/06.ts
+++ days/06/06.ts
@@ -1,6 +1,102 @@
-import { Answer } from "../../src/types.ts";
+import { diffStr } from "jsr:@std/internal@^1.0.5/diff-str";
+import { Answer, open } from "../../src/types.ts";
 
 /** Provides result output of day done */
 export function result(): Answer {
   return {};
+}
+
+// alias for position facing direction
+type Position = [x: number, y: number, facing: string];
+
+// Guard facing to direction
+const GuardDirection: Map<string, Position> = new Map([
+  ["^", [0, 1, "^"]],
+  [">", [1, 0, ">"]],
+  ["v", [0, -1, "v"]],
+  ["<", [-1, 0, "<"]],
+]);
+
+/** Question questioning */
+export class Question {
+  theMap: TheMap;
+
+  constructor(file: string) {
+    this.theMap = new TheMap(open(file));
+  }
+
+  sum(): number {
+    return 0;
+  }
+}
+
+/** struct for handling map motion */
+class TheMap {
+  /** the raw map without changing */
+  private raw: Array<Array<string>> = [];
+  /** the map identifying the route taken */
+  private route: Array<Array<number>> = [];
+  /** the direction of the guard */
+  private direction: Position = [0, 0, ""];
+  /** the position of the guard */
+  private position: Position = [0, 0, ""];
+
+  /**
+   * @param lines the lines to be digested
+   */
+  constructor(lines: string[]) {
+    // set the row to know where starting
+    let row = 0;
+
+    lines.map((line) => {
+      const maybe = this.findDirection(line);
+
+      if (maybe != undefined) {
+        let column: number = 0;
+        [this.direction, column] = maybe;
+        this.position = [row, column, this.direction[2]];
+      }
+
+      this.raw.push(line.split(""));
+    });
+    row++;
+  }
+
+  /**
+   * get the direction of guard
+   * @param line the line to be checked
+   */
+  private findDirection(
+    line: string,
+  ): [direction: Position, index: number] | undefined {
+    const facingRaw = /\^|>|v|</;
+    const maybe = line.match(facingRaw);
+
+    if (maybe?.index != undefined) {
+      // location on where to start
+      const direction = GuardDirection.get(maybe[0]);
+      const index = line.match(facingRaw)?.index;
+      if (index != undefined && direction != undefined) {
+        return [direction, index];
+      }
+    }
+
+    return undefined;
+  }
+
+  /**
+   * rotate the direction of the guard
+   */
+  private rotateDirection(facing: string): Position | undefined {
+    switch (facing) {
+      case "^":
+        return GuardDirection.get(">");
+      case ">":
+        return GuardDirection.get("v");
+      case "v":
+        return GuardDirection.get("<");
+      case "<":
+        return GuardDirection.get("^");
+    }
+  }
 }
--- days/06/06_test.ts
+++ days/06/06_test.ts
@@ -1,6 +1,18 @@
 import { assertEquals } from "jsr:@std/assert";
 import { result } from "./06.ts";
 
+const sample = "./samples/06.txt";
+
 Deno.test("can get result", () => {
   assertEquals(result(), {});
+});
+
+Deno.test("can traverse 90%", () => {
+  const expect = 41;
+
+  const question = new Question(sample);
+
+  const actual = question.sum();
+
+  assertEquals(actual, expect);
 });
